e2252d1fd00cdd075c7172a65782d7acfd70b58d
Juan Prado <juanprado.ux@gmail.com>
2019-08-18 16:07:29 -0400

    first commit, starting splash page

---
+++ src/views/components/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Splash = () => (
+  <section className="splash">
+    <div className="splash-content">
+      <p className="splash-text">Katie is a <br />product designer <br /><i>and</i> art director</p>
+      <span className="splash-info">NYC</span>
+    </div>
+  </section>
+);
+
+export default Splash;
---
+++ src/views/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Splash from './components/Splash';
+
+const Index = () => (
+  <Splash />
+);
+
+ReactDOM.render(<Index />, document.getElementById('index'));
